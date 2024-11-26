2d039efdca17a2b27736b6dc3fc482a3f3e16126
Nigel Petargue <nigelpetargue@gmail.com>
2024-11-26 20:18:51 +0800

    change position

--- utils/experience.ts
+++ utils/experience.ts
@@ -2,30 +2,30 @@
 
 export const experience = [
   {
-    title: "JeonSoft Corporation",
-    position: "Junior Software Developer",
-    time: "December 11, 2023 - Present",
-    location: "Quezon City, National Capital Region",
+    title: 'JeonSoft Corporation',
+    position: 'Software Developer',
+    time: 'December 11, 2023 - Present',
+    location: 'Quezon City, National Capital Region',
     description:
-      "Empower teams through collaborative problem-solving and innovative thinking, leveraging React, Node.js, PostgreSQL, and Knex.js to deliver high-quality software solutions that exceed client expectations.",
-    tech: ["React", "NodeJS", "PostgreSQL", "KnexJS"],
+      'Empower teams through collaborative problem-solving and innovative thinking, leveraging React, Node.js, PostgreSQL, and Knex.js to deliver high-quality software solutions that exceed client expectations.',
+    tech: ['React', 'NodeJS', 'PostgreSQL', 'KnexJS'],
   },
   {
-    title: "Wela Online System",
-    position: "Junior Web Developer(Intern)",
-    time: "August - October 2022",
-    location: "Ideaspace 172 Salcedo, Legazpi Village, Makati City, Philippines",
+    title: 'Wela Online System',
+    position: 'Junior Web Developer(Intern)',
+    time: 'August - October 2022',
+    location: 'Ideaspace 172 Salcedo, Legazpi Village, Makati City, Philippines',
     description:
-      "Engage in close collaboration with both the design and development teams to foster a cohesive and synergistic workflow that ensures the successful realization of projects.",
-    tech: ["React", "Typescript", "NextJS", "Redux", "Firebase"],
+      'Engage in close collaboration with both the design and development teams to foster a cohesive and synergistic workflow that ensures the successful realization of projects.',
+    tech: ['React', 'Typescript', 'NextJS', 'Redux', 'Firebase'],
   },
   {
-    title: "San Benissa Garden Villa",
-    position: "Desktop Support Technician(Intern)",
-    time: "October - December 2017",
-    location: "G, 0 Commonwealth Ave, Novaliches, Quezon City, Metro Manila",
+    title: 'San Benissa Garden Villa',
+    position: 'Desktop Support Technician(Intern)',
+    time: 'October - December 2017',
+    location: 'G, 0 Commonwealth Ave, Novaliches, Quezon City, Metro Manila',
     description:
-      " Provide prompt technical assistance, troubleshooting and  maintaining network infrastructure for seamless operations.  Customer centric approach ensured efficient IT service for resident staff.",
-    tech: ["WordPress", "Microsoft Excel", "VBA", "Networking"],
+      ' Provide prompt technical assistance, troubleshooting and  maintaining network infrastructure for seamless operations.  Customer centric approach ensured efficient IT service for resident staff.',
+    tech: ['WordPress', 'Microsoft Excel', 'VBA', 'Networking'],
   },
 ]
