4f8a360c95f980f2fe0c8c43b03986ec501ab888
Nigel Petargue <nigelpetargue@gmail.com>
2024-12-27 19:44:01 +0800

    add evo gym project

--- utils/projects.tsx
+++ utils/projects.tsx
@@ -1,5 +1,26 @@
 export const projects = [
   {
+    title: 'EvoGym',
+    imgSrc: '/Screenshot 2024-12-27 193712.png',
+    code: 'https://evo-gym-three.vercel.app/',
+    projectLink: 'https://evo-gym-three.vercel.app/',
+    tech: ['React', 'Vite', 'Tailwindcss', 'Typescript', 'Framer Motion', 'Vercel'],
+    description: 'Your destination for premium cosmetics and skin-whitening products.',
+    modalContent: (
+      <>
+        <p>
+          Unrivaled Gym. Unparalleled Training Fitness Classes. World Class Studios to get
+          the Body Shapes That you Dream of.. Get Your Dream Body Now.
+        </p>
+        <p>
+          Building on my experience with Evo Gym, I prioritized creating intuitive
+          navigation and visually engaging layouts to effectively showcase services and
+          help users find exactly what they need.
+        </p>
+      </>
+    ),
+  },
+  {
     title: 'Glowing',
     imgSrc: '/glowing.png',
     code: 'https://ecommerce-glowing-one.vercel.app/',
@@ -9,22 +30,26 @@ export const projects = [
     modalContent: (
       <>
         <p>
-          I independently designed and developed an e-commerce platform that delivers a seamless shopping experience.
+          I independently designed and developed an e-commerce platform that delivers a
+          seamless shopping experience.
         </p>
         <p>
-          Inspired by my work on Glowing, I focused on intuitive navigation and visually appealing layouts to effectively showcase products and connect buyers with what they need.
+          Inspired by my work on Glowing, I focused on intuitive navigation and visually
+          appealing layouts to effectively showcase products and connect buyers with what
+          they need.
         </p>
       </>
     ),
-    
   },
   {
     title: 'Sclique E-commerce',
     imgSrc: '/sclique.png',
     code: 'https://sclique-chrisnics.vercel.app/?fbclid=IwAR2Dc5dICnBbPSlu11DHvHshxtPLAR7y_JgVmfljBgjc3o9MgKdP9CzTBIw',
-    projectLink: 'https://sclique-chrisnics.vercel.app/?fbclid=IwAR2Dc5dICnBbPSlu11DHvHshxtPLAR7y_JgVmfljBgjc3o9MgKdP9CzTBIw',
+    projectLink:
+      'https://sclique-chrisnics.vercel.app/?fbclid=IwAR2Dc5dICnBbPSlu11DHvHshxtPLAR7y_JgVmfljBgjc3o9MgKdP9CzTBIw',
     tech: ['React', 'Nextjs', 'Mongoose', 'Zustand', 'Mantine', 'Vercel'],
-    description: 'Discover Sclique E-commerce: Home to Stylish Window Accordions Nationwide.',
+    description:
+      'Discover Sclique E-commerce: Home to Stylish Window Accordions Nationwide.',
     modalContent: (
       <>
         <p>
@@ -32,8 +57,8 @@ export const projects = [
           e-commerce platform.
         </p>
         <p>
-          Leveraging my web development skills, we crafted a user-friendly website connecting buyers
-          and sellers of windows blinds and upholstery products.
+          Leveraging my web development skills, we crafted a user-friendly website
+          connecting buyers and sellers of windows blinds and upholstery products.
         </p>
       </>
     ),
@@ -48,11 +73,13 @@ export const projects = [
     modalContent: (
       <>
         <p>
-          I try to work as a freelance and paid to create a sample landing page for their travel
-          agency business.
+          I try to work as a freelance and paid to create a sample landing page for their
+          travel agency business.
         </p>
         <p>It was just a front end design just to showcase my creativity.</p>
-        <p>I use tools that i know i can use to provide a quality project to my client.</p>
+        <p>
+          I use tools that i know i can use to provide a quality project to my client.
+        </p>
       </>
     ),
   },
@@ -66,12 +93,13 @@ export const projects = [
     modalContent: (
       <>
         <p>
-          This project is an implementation of a shopping cart using TypeScript, React, Vite,
-          Tailwind CSS, and Mantine. It ensures type safety and code quality with TypeScript and
-          utilizes React for building the user interface. Vite optimizes the development and build
-          process, while Tailwind CSS provides easy and customizable styling.
+          This project is an implementation of a shopping cart using TypeScript, React,
+          Vite, Tailwind CSS, and Mantine. It ensures type safety and code quality with
+          TypeScript and utilizes React for building the user interface. Vite optimizes
+          the development and build process, while Tailwind CSS provides easy and
+          customizable styling.
         </p>
       </>
     ),
   },
-];
+]
